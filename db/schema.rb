0a740e36949267712fca81e0f40ee1cea1b06e1e
PARDEEP KAUR <pkaur84@aol.com>
2020-06-11 01:40:52 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,10 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.integer "employees_count"
+    t.integer "num_of_employees"
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "costumes", force: :cascade do |t|
